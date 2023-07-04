0a92c0e3218b78d214183a5e36b6de99dba1ad17
StarisbornT <127780988+StarisbornT@users.noreply.github.com>
2023-07-04 04:20:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///blog.db"
 #postgres://blog:28TUl9nXzhdxkwWec7vJqS4lXrjmG9u8@dpg-chf0keu4dad1jq99ae80-a.oregon-postgres.render.com/blog_1ld7
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SERVER_NAME'] = 'https://cryptowizard101.onrender.com'
+app.config['PREFERRED_URL_SCHEME'] = 'https'
+app.config['APPLICATION_ROOT'] = '/'
+
 db = SQLAlchemy(app)
 app.app_context().push()
 
